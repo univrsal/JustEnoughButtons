611c0dbaf98cde1dd41ce48eb39954f57670d82f
universallp <universailp@web.de>
2017-06-06 14:01:00 +0200

    Version number

--- src/main/java/de/universallp/justenoughbuttons/JEIButtons.java
+++ src/main/java/de/universallp/justenoughbuttons/JEIButtons.java
@@ -26,7 +26,7 @@ import org.apache.logging.log4j.Level;
 public class JEIButtons {
 
     public static final String MODID = "justenoughbuttons";
-    static final String VERSION = "1.11.2-1.4.5";
+    static final String VERSION = "1.11.2-1.4.6";
     public static final String MOD_MOREOVERLAYS = "moreoverlays";
     public static final String MOD_DYN_SOURROUND = "dsurround";
     public static final String MOD_JEI = "jei";
